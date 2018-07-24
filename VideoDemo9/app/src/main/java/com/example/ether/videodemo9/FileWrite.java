9198e8600fe4fb7eeacf1a2180a606c8558618f3
yetote <503779938@qq.com>
2018-07-24 17:36:56 +0800

    VideoDemo9
    编码h264完成

---
+++ VideoDemo9/app/src/main/java/com/example/ether/videodemo9/FileWrite.java
@@ -0,0 +1,53 @@
+package com.example.ether.videodemo9;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+public class FileWrite {
+    private ByteBuffer buffer;
+    private int size;
+    private FileChannel channel;
+    String path;
+    private FileOutputStream fos;
+
+    public FileWrite(String path) {
+        this.path = path;
+        File file = new File(path);
+        try {
+            if (!file.exists()) {
+                file.getParentFile().mkdir();
+                file.createNewFile();
+            }
+            fos = new FileOutputStream(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        channel = fos.getChannel();
+    }
+
+    public void writeData(byte[] data) {
+        buffer = ByteBuffer.allocate(data.length);
+        buffer.put(data);
+        buffer.flip();
+        try {
+            channel.write(buffer);
+            buffer.clear();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void close() {
+        try {
+            fos.close();
+            channel.close();
+            buffer.clear();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
--- VideoDemo9/app/src/main/java/com/example/ether/videodemo9/MainActivity.java
+++ VideoDemo9/app/src/main/java/com/example/ether/videodemo9/MainActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
 import android.graphics.ImageFormat;
+import android.graphics.Rect;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCaptureSession;
 import android.hardware.camera2.CameraDevice;
@@ -29,6 +30,7 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
@@ -37,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
     public static final int PERMISSION_CAMERA_CODE = 1;
     private SurfaceView surfaceView;
     private SurfaceHolder surfaceHolder;
-    private Button btn;
+    private Button btn, stop;
     private String cameraId;
     private HandlerThread handlerThread;
     private CameraCaptureSession cameraCaptureSession;
@@ -46,33 +48,36 @@ public class MainActivity extends AppCompatActivity {
     private MediaCodec mediaCodec;
     int i = 0;
     private static final String TAG = "MainActivity";
-    public static final String MIME_TYPE = "Video/AVC";
+    public static final String MIME_TYPE = "video/avc";
+    private FileWrite write;
+    String path;
+    private boolean isVideoing = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
-        initEncode();
 
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                startVideo();
-            }
+        btn.setOnClickListener(v -> {
+            isVideoing = true;
+            startVideo();
         });
+        stop.setOnClickListener(v -> isVideoing = false);
     }
 
-    private void initEncode() {
+    private void initEncode(int width, int height) {
         try {
             mediaCodec = MediaCodec.createEncoderByType(MIME_TYPE);
-            MediaFormat format = new MediaFormat();
-            format.setInteger(MediaFormat.KEY_BIT_RATE, 256000);
-            format.setInteger(MediaFormat.KEY_FRAME_RATE, 15);
+            MediaFormat format = MediaFormat.createVideoFormat("video/avc", 800, 600);
+//            byte[] csd_info = {0, 0, 0, 1, 103, 100, 0, 40, -84, 52, -59, 1, -32, 17, 31, 120, 11, 80, 16, 16, 31, 0, 0, 3, 3, -23, 0, 0, -22, 96, -108, 0, 0, 0, 1, 104, -18, 60, -128};
+//            format.setByteBuffer("csd-0", ByteBuffer.wrap(csd_info));
+            format.setInteger(MediaFormat.KEY_BIT_RATE, 800 * 600 * 5);
+            format.setInteger(MediaFormat.KEY_FRAME_RATE, 30);
             format.setInteger(MediaFormat.KEY_COLOR_FORMAT, MediaCodecInfo.CodecCapabilities.COLOR_FormatYUV420Flexible);
-            format.setInteger(MediaFormat.KEY_I_FRAME_INTERVAL, 5);
-            mediaCodec.configure(format,null,null,MediaCodec.CONFIGURE_FLAG_ENCODE);
+            format.setInteger(MediaFormat.KEY_I_FRAME_INTERVAL, 1);
+            format.setInteger(MediaFormat.KEY_MAX_INPUT_SIZE, 1280 * 720 * 10);
+            mediaCodec.configure(format, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE);
             mediaCodec.start();
         } catch (IOException e) {
             e.printStackTrace();
@@ -154,21 +159,26 @@ public class MainActivity extends AppCompatActivity {
 
     private void init() {
         btn = findViewById(R.id.btn);
+        stop = findViewById(R.id.stop);
         surfaceView = findViewById(R.id.surfaceView);
         surfaceHolder = surfaceView.getHolder();
         surfaceHolder.addCallback(new SurfaceHolder.Callback() {
             @Override
             public void surfaceCreated(SurfaceHolder holder) {
-                imageReader = ImageReader.newInstance(surfaceView.getWidth(), surfaceView.getHeight(), ImageFormat.YUV_420_888, 1);
-                imageReader.setOnImageAvailableListener(new ImageReader.OnImageAvailableListener() {
-                    @Override
-                    public void onImageAvailable(ImageReader reader) {
-                        Image image = reader.acquireNextImage();
-                        i++;
-                        Log.e(TAG, "onImageAvailable: " + i);
-                        image.close();
-                        encodeH264();
-                    }
+                initEncode(1280/2, 720/2);
+
+                imageReader = ImageReader.newInstance(surfaceView.getWidth()/2, surfaceView.getHeight()/2, ImageFormat.YUV_420_888, 1);
+                imageReader.setOnImageAvailableListener(reader -> {
+                    Image image = reader.acquireNextImage();
+                    i++;
+                    Log.e(TAG, "onImageAvailable: " + i);
+//                    ByteBuffer buffer = image.getPlanes()[0].getBuffer();
+                    byte[] temp = getDataFromImage(image);
+//                    buffer.get(temp);
+                    encodeH264(temp);
+//                    write.writeData(getDataFromImage(image));
+                    Log.e(TAG, "surfaceCreated: " + surfaceView.getWidth() + surfaceView.getHeight());
+                    image.close();
                 }, new Handler(getMainLooper()));
                 if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
                     ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, PERMISSION_CAMERA_CODE);
@@ -190,10 +200,37 @@ public class MainActivity extends AppCompatActivity {
         handlerThread = new HandlerThread("camera2");
         handlerThread.start();
         handler = new Handler(handlerThread.getLooper());
+        path = this.getExternalCacheDir().getPath() + "/test.h264";
+        write = new FileWrite(path);
     }
 
     private void encodeH264(byte[] data) {
-        
+        int index = mediaCodec.dequeueInputBuffer(System.nanoTime());
+        if (index >= 0) {
+            ByteBuffer inputBuffer = mediaCodec.getInputBuffer(index);
+            inputBuffer.clear();
+            if (data.length > 0) {
+                Log.e(TAG, "encodeH264: " + data.length);
+                Log.e(TAG, "encodeH264: " + inputBuffer.capacity());
+                inputBuffer.put(data);
+                mediaCodec.queueInputBuffer(index, 0, data.length, System.nanoTime() / 1000, MediaCodec.BUFFER_FLAG_CODEC_CONFIG);
+            }
+        }
+
+        MediaCodec.BufferInfo bufferInfo = new MediaCodec.BufferInfo();
+        int outIndex;
+        do {
+            outIndex = mediaCodec.dequeueOutputBuffer(bufferInfo, 0);
+            if (outIndex >= 0) {
+                ByteBuffer outputBuffer = mediaCodec.getOutputBuffer(outIndex);
+                outputBuffer.position(bufferInfo.offset);
+                byte[] temp = new byte[bufferInfo.size];
+                outputBuffer.get(temp);
+
+                write.writeData(temp);
+                mediaCodec.releaseOutputBuffer(outIndex, false);
+            }
+        } while (outIndex >= 0);
     }
 
 
@@ -209,5 +246,67 @@ public class MainActivity extends AppCompatActivity {
                 }
                 break;
         }
+    }
+
+    private static byte[] getDataFromImage(Image image) {
+        Rect crop = image.getCropRect();
+        int format = image.getFormat();
+        int width = crop.width();
+        int height = crop.height();
+        Image.Plane[] planes = image.getPlanes();
+        byte[] data = new byte[width * height * ImageFormat.getBitsPerPixel(format)/8];
+        Log.e(TAG, "getDataFromImage: data.length " + ImageFormat.getBitsPerPixel(format));
+        byte[] rowData = new byte[planes[0].getRowStride()];
+        int channelOffset = 0;
+        int outputStride = 1;
+        for (int i = 0; i < planes.length; i++) {
+            switch (i) {
+                case 0:
+                    channelOffset = 0;
+                    outputStride = 1;
+                    break;
+                case 1:
+                    channelOffset = width * height + 1;
+                    outputStride = 2;
+                    break;
+                case 2:
+                    channelOffset = width * height;
+                    outputStride = 2;
+                    break;
+            }
+            ByteBuffer buffer = planes[i].getBuffer();
+
+            int rowStride = planes[i].getRowStride();
+            int pixelStride = planes[i].getPixelStride();
+            Log.v(TAG, "pixelStride " + pixelStride);
+            Log.v(TAG, "rowStride " + rowStride);
+            Log.v(TAG, "width " + width);
+            Log.v(TAG, "height " + height);
+            Log.v(TAG, "buffer size " + buffer.remaining());
+
+            int shift = (i == 0) ? 0 : 1;
+            int w = width >> shift;
+            int h = height >> shift;
+            buffer.position(rowStride * (crop.top >> shift) + pixelStride * (crop.left >> shift));
+            for (int row = 0; row < h; row++) {
+                int length;
+                if (pixelStride == 1 && outputStride == 1) {
+                    length = w;
+                    buffer.get(data, channelOffset, length);
+                    channelOffset += length;
+                } else {
+                    length = (w - 1) * pixelStride + 1;
+                    buffer.get(rowData, 0, length);
+                    for (int col = 0; col < w; col++) {
+                        data[channelOffset] = rowData[col * pixelStride];
+                        channelOffset += outputStride;
+                    }
+                }
+                if (row < h - 1) {
+                    buffer.position(buffer.position() + rowStride - length);
+                }
+            }
+        }
+        return data;
     }
 }
